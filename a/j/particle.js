7b09943eaaaced55e4c2929288aeab410b584134
Tim Huegdon <tim@nefariousdesigns.co.uk>
2010-11-15 22:48:52 +0000

    ParticleCluster changes.

--- a/j/particle.js
+++ a/j/particle.js
@@ -1,10 +1,12 @@
-var Particle = function(c, x, y, size, col) {
+var Particle = function(c, x, y, size, col, xVel, yVel) {
     this.c = c;
     this.ctx = this.c.getContext('2d');
     this.x = x;
     this.y = y;
 	this.size = size;
     this.col = col || "rgba(255, 255, 255, 0.5)";
+	this.xVel = xVel;
+	this.yVel = yVel * -1;
     
     return this;
 };
@@ -26,16 +28,17 @@ Particle.prototype = {
 	}
 };
 
-var ParticleCluster = function(canvas, x, y) {
+var ParticleCluster = function(canvas, x, y, size, col) {
 	this.canvas = canvas;
 	this.context = canvas.getContext('2d');
 	this.originX = x;
 	this.originY = y;
-	this.particleSize = 3;
+	this.colour = col || NEF.tools.rgbaString(255, 255, 255, 1);
+	this.particleSize = size || 1;
 	this.gravity = 0.08;
 	this.drag = 0.999999;
 	this.bounceDecay = 0.6;
-	this.limit = 200;
+	this.limit = 400;
 	this.count = 0;
 	this.particles = new Array(this.limit);
 	
@@ -51,21 +54,16 @@ ParticleCluster.prototype = {
             this.originX,
             this.originY,
             this.particleSize,
-            NEF.tools.rgbaString(
-                NEF.random.getRandomInt(255, 125),
-                NEF.random.getRandomInt(255, 125),
-                NEF.random.getRandomInt(255, 125),
-                0.75
-            )
+            this.colour,
+			NEF.random.getRandomArbitrary(-3, 3),
+			NEF.random.getRandomArbitrary(4, 0)
         );
         
         for (var i in this.particles) {
             this.particles[i].render();
 			
-			var yVel = (this.particles[i].yVel + this.gravity) * this.drag
-					|| NEF.random.getRandomArbitrary(4, 0) * -1,
-				xVel = this.particles[i].xVel * this.drag
-					|| NEF.random.getRandomArbitrary(-3, 3),
+			var yVel = (this.particles[i].yVel + this.gravity) * this.drag,
+				xVel = this.particles[i].xVel * this.drag,
 				x = this.particles[i].x,
 				y = this.particles[i].y;
 
